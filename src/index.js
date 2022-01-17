3f7e1d839b725760c45a8b818c60d162b8ba93d7
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-17 18:39:08 +0100

    Uploading project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,
+    document.getElementById('root'));
+
+//ReactDOM.render(<CardList2 />,
+//    document.getElementById('root'));
+
+//ReactDOM.render(<CardList1 />,
+  //  document.getElementById('root'));
+
+//Ako se napravi vaka vtoriot call go overridnuva prviot
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/robots.js
@@ -0,0 +1,6 @@
+export const robots = [
+    {name: 'Marko Bachvarovski', email: 'marko.bachvarovski@gmail.com'},
+    {name:'Mia Velova', email: 'mia.velova@gmail.com'},
+    {name: 'Mitre', email: 'mitre@gmail.com'},
+    {name: 'Andrej Bachvarovski', email: 'andrejbach@gmail.com'}
+];
